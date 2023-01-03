87309ba44ce716709d72cfe03afed391b96602b3
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2023-01-03 14:42:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
-
 #include <iostream>
+
 using namespace std;
 
-int main() {
+int greatest_common_divisor(int a, int b) {
+  if (a % b == 0)
+    return b;
+  if (b % a == 0)
+    return a;
+  if (a > b)
+    return greatest_common_divisor(a%b, b);
+  return greatest_common_divisor(a, b%a);
+}
+
+int **set_array(int width,int height) {
     
-    cout << "Тест";
+}
+
+int main()
+{
+    int **array;
+    int a,b;
+    cout<<"Введите длину, высоту системы:\n";
+    cin >> a >> b;
+    
+    //cout << greatest_common_divisor(a,b);
+    
+    
+    
+
     return 0;
 }
