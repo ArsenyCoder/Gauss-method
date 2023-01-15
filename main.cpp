03eb5fa6e1decc2a13eabcb84997ac346c3b783a
ArsenyCoder <114302368+ArsenyCoder@users.noreply.github.com>
2023-01-15 11:44:58 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,84 @@
-#include <iostream>
-
+#include<iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <string>
 using namespace std;
-
-int greatest_common_divisor(int a, int b) {
-  if (a % b == 0)
-    return b;
-  if (b % a == 0)
-    return a;
-  if (a > b)
-    return greatest_common_divisor(a%b, b);
-  return greatest_common_divisor(a, b%a);
-}
-
-int **set_array(int width,int height) {
-    
-}
-
+ 
 int main()
 {
-    int **array;
-    int a,b;
-    cout<<"Введите длину, высоту системы:\n";
-    cin >> a >> b;
-    
-    //cout << greatest_common_divisor(a,b);
-    
-    
-    
+   int i, j, n, m;
+   cout << "введи число уравнений: ";
+         cin >> n;
+         cout << "введи число неизвестных: ";
+         cin >> m;
+         m+=1;
+    float **matrix = new float *[n];
+         for (i=0; i<n; i++)
+             matrix[i] = new float [m];
+ 
+//инициализируем
+ 
+    for (i = 0; i<n; i++)
+ 
+        for (j = 0; j<m; j++)
+        {
+            cout << "Элемент " << "[" << i+1 << " , " << j+1 << "]: "  ;
+ 
+            cin >> matrix[i][j];
+}
+ 
+ //выводим массив
+    cout << "matrix: " << endl;
+      for (i=0; i<n; i++)
+       {
+          for (j=0; j<m; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
+       }
+    cout << endl;
+ 
+//Метод Гаусса
+//Прямой ход, приведение к верхнетреугольному виду
+    float  tmp, xx[m];
+    int k;
+ 
+    for (i=0; i<n; i++)
+     {
+       tmp=matrix[i][i];
+         for (j=n;j>=i;j--)
+             matrix[i][j]/=tmp;
+           for (j=i+1;j<n;j++)
+          {
+             tmp=matrix[j][i];
+               for (k=n;k>=i;k--)
+             matrix[j][k]-=tmp*matrix[i][k];
+          }
+      }
 
-    return 0;
+    xx[n-1] = matrix[n-1][n];
+     for (i=n-2; i>=0; i--)
+       {
+           xx[i] = matrix[i][n];
+           for (j=i+1;j<n;j++) xx[i]-=matrix[i][j]*xx[j];
+       }
+
+        bool correct = true;
+        for (i=0; i<n; i++) {
+            if (to_string(xx[i]) == "-nan") {
+                correct = false;
+            }
+        }
+
+        if (correct) {
+            for (i=0; i<n; i++) {
+                cout << "X" << i + 1  << " = " << xx[i] << endl;
+            }
+            cout << endl;
+        } else {
+            cout << endl << "Нет решений!";
+        }
+ 
+    delete[] matrix;
+ 
+   return 0;
 }
